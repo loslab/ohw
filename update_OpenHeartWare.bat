6f00a7f112c37ea81de1b2749e6c90d2a96ad40d
carlasailer <40063163+carlasailer@users.noreply.github.com>
2019-04-04 10:27:45 +0200

    update-bat file available incl create_config_file
    Signed-off-by: carlasailer <40063163+carlasailer@users.noreply.github.com>

---
+++ update_OpenHeartWare.bat
@@ -0,0 +1,11 @@
+@echo off
+REM SET VARIABLES
+SET anaconda_folder=C:\ProgramData\Anaconda3\Scripts\activate.bat
+
+REM UPDATE THE APPLICATION
+windir%\System32\cmd.exe "/K" 
+@echo on
+call "%anaconda_folder%"
+call conda env update -f=ohw.yml
+call python libraries\create_config_file.py
+pause
